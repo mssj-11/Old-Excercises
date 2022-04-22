1d0d3dccabe649efdcc7389bb1cf41b12747b12e
Michael Salgado Sotero <80673026+mssj-11@users.noreply.github.com>
2022-04-21 22:56:09 -0500

    SCHOOL exercises

---
+++ Java/school_exercises/Decode-Transposition-master/src/trans/Decodification.java
@@ -0,0 +1,76 @@
+package trans;
+
+import java.util.*;
+
+public class Decodification {
+
+    private static String printTable(char[][] columnArray, int[] key) {
+        String res = "";
+        for (char[] columnArray1 : columnArray) {
+            for (int i = 0; i < columnArray1.length; i++) {
+                res += columnArray1[key[i]];
+
+            }
+        }
+        return res;
+    }
+
+    public static String decode(int[] permutation, int[] overflows, String text) {
+
+        char[][] table = new char[rowsCalculator(text, permutation.length)][permutation.length];
+
+        for (int i = 0; i < permutation.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (overflows[i] == 0 && j == table.length - 1) {
+                    table[j][i] = '-';
+                } else {
+                    table[j][i] = text.charAt(0);
+                    text = text.substring(1);
+                }
+
+            }
+
+        }
+        return printTable(table, permutation);
+
+    }
+
+    private static int rowsCalculator(String text, int colums) {
+        final int rows = text.length() / colums;
+        return text.length() % colums > 0 ? rows + 1 : rows;
+    }
+
+    protected Character[][] DecodeWithBruteForce() {
+        return null;
+    }
+
+    private ArrayList<Integer> KeyTransformation(String key) {
+        ArrayList<Integer> transformation = new ArrayList<>();
+        ArrayList<Integer> result;
+        String keyToUpper = key.toUpperCase();
+
+        for (int i = 0; i < keyToUpper.length(); i++) {
+            transformation.add((int) keyToUpper.charAt(i) - 64);
+        }
+
+        result = (ArrayList<Integer>) transformation.clone();
+
+        for (int i = 0; i < key.length(); i++) {
+            int position = MaxValue(transformation);
+            transformation.set(position, 0);
+            result.set(position, key.length() - (i + 1));
+        }
+        return result;
+
+    }
+
+    private int MaxValue(ArrayList<Integer> transformation) {
+        int pointer = 0;
+        for (int i = transformation.size() - 1; i > 0; i--) {
+            if (transformation.get(i) > transformation.get(pointer)) {
+                pointer = i;
+            }
+        }
+        return pointer;
+    }
+}
---
+++ Java/school_exercises/Decode-Transposition-master/src/trans/InputData.java
@@ -0,0 +1,28 @@
+package trans;
+
+import java.util.Scanner;
+
+public class InputData {
+
+    private String cipherText;
+    private final String possibleWord;
+
+    public InputData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert cipher text:");
+        this.cipherText = scanner.nextLine();
+        System.out.println("Insert probable word:");
+        this.possibleWord = scanner.nextLine();
+        System.out.println("DECIPHERING...");
+    }
+
+    public String getCipherText() {
+        cipherText = cipherText.replaceAll(" ", "");
+        return cipherText.toUpperCase();
+    }
+
+    public String getPosibleWord() {
+        return possibleWord.toUpperCase();
+    }
+
+}
