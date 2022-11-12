84c0e9b4c94eaf88918bfbad16cf9711e9dd33e4
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-11 23:57:50 -0600

    Fechas en java

---
+++ Java/POO_Java2022/src/fechas/JCalendarEjemplo.java
@@ -0,0 +1,10 @@
+package fechas;     //  Ejemplo 4: JCalendar    #Part 1
+/**
+ * @author HP
+ */
+public class JCalendarEjemplo {
+    public static void main(String[] args) {
+        JCalendar_ejemplo ventanaJC = new JCalendar_ejemplo();
+        ventanaJC.setVisible(true);
+    }
+}
