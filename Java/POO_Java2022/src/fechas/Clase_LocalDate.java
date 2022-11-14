c323ddb432913ecfba81da6be57f84596c0519e3
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-13 20:17:28 -0600

    Fechas en java

---
+++ Java/POO_Java2022/src/fechas/Clase_LocalDate.java
@@ -0,0 +1,10 @@
+//  Ejemplo 9: Clase LocalDate (.now(), .isBefore(), .isAfter())
+package fechas;
+
+import java.time.LocalDate;
+
+public class Clase_LocalDate {
+    public static void main(String[] args) {
+        LocalDate fecha = LocalDate.now();
+    }
+}
