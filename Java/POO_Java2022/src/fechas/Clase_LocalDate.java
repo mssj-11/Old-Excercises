2381aedc053645d63394473174d086058a20fee3
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-13 20:26:09 -0600

    Fechas en java

--- Java/POO_Java2022/src/fechas/Clase_LocalDate.java
+++ Java/POO_Java2022/src/fechas/Clase_LocalDate.java
@@ -2,9 +2,24 @@
 package fechas;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 public class Clase_LocalDate {
     public static void main(String[] args) {
-        LocalDate fecha = LocalDate.now();
+        LocalDate fechaActual = LocalDate.now();
+        LocalDate fechaAnterior = LocalDate.of(2022, 11, 12);
+        
+        System.out.println("Fecha Actual: " +fechaActual);
+        System.out.println("Fecha Anterior: " +fechaAnterior);
+        
+        
+        //  Comparando fechas
+        if(fechaAnterior.isBefore(fechaActual)){
+            System.out.println("La fecha ya paso");
+        }else{
+            System.out.println("Aun faltan dias");
+        }
+        
+        
     }
 }
