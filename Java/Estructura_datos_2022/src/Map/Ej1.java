44d44248655211e154131033db7a617e887ba14b
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-16 20:57:27 -0600

    Estructura Map

---
+++ Java/Estructura_datos_2022/src/Map/Ej1.java
@@ -0,0 +1,23 @@
+//  Ejemplo 1: Map
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        
+        //  Creacion de un Map
+        Map<Integer, String> cripto = new HashMap<>(); 
+        
+        cripto.put(1, "Bitcoin");
+        cripto.put(2, "Ethereum");
+        cripto.put(3, "Binance");
+        cripto.put(4, "Cardano");
+        cripto.put(5, "Solana");
+        
+        
+        
+        
+    }
+}
