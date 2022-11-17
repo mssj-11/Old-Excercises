6e28cbe8652a884c530601be33156a3dbe25c988
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-16 22:48:56 -0600

    Estructura Map

--- Java/Estructura_datos_2022/src/Map/Ej1.java
+++ Java/Estructura_datos_2022/src/Map/Ej1.java
@@ -28,6 +28,31 @@ public class Ej1 {
         System.out.println("Esta vacio? " +cripto.isEmpty());   //  Esta vacio? false
         //  Tamaño del Map
         System.out.println("El tamaño del Map es de: " +cripto.size()); //  El tamaño del Map es de: 5
+        //  El Map cointiene una clave en especifico
+        System.out.println("Se encuentra esta clave? " +cripto.containsKey(3)); //  Se encuentra esta clave? true
+        //  Si existe un valor
+        System.out.println("Este valor existe? " +cripto.containsValue("Bitcoin")); //  Este valor existe? true
+        //  Si no existe un valor - Remplazar con otro(por defecto)
+        System.out.println("Esta este valor? " +cripto.getOrDefault(1, "Bit")); //  Esta este valor? Bitcoin
+        System.out.println("Esta este valor? " +cripto.getOrDefault(8, "Bit")); //  Esta este valor? Bit    Ya que no existe
+        
+        
+        //  Igualdades con otro Map
+        Map<Integer, String> cripto2 = new HashMap<>(); 
+        cripto2.put(1, "Bitcoin");
+        cripto2.put(2, "Ethereum");
+        cripto2.put(3, "Binance");
+        cripto2.put(4, "Cardano");
+        cripto2.put(5, "Solana");
+        //  Tienen la misma cantidad de datos y los mismos datos
+        System.out.println("\n\nSon iguales? " +cripto.equals(cripto2));    //  Son iguales? true
+        
+        
+        
+        
+        System.out.println("\n\nCiclo forEach");
+        //  Ejemplo con un ciclo forEach    (Clave y Valores)
+        cripto.forEach((c, v) -> System.out.println("Clave: "+c+ "Valor: "+v)); // Funcion Lambda
         
     }
 }
