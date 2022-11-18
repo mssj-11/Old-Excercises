cb1d47bf45de747007dd1fd342c3d3034c41a6e2
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-17 22:26:36 -0600

    Estructura Map

--- Java/Estructura_datos_2022/src/Map/Recorrer_Map.java
+++ Java/Estructura_datos_2022/src/Map/Recorrer_Map.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//  Ejemplo 2: Formas de recorrer un Map
 package Map;
 
-/**
- *
- * @author HP
- */
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Recorrer_Map {
-    
+    public static void main(String[] args) {
+        
+        Map<Integer, String> cripto = new HashMap<>();
+        
+        cripto.put(1, "Bitcoin");
+        cripto.put(2, "Ethereum");
+        cripto.put(3, "Binance");
+        cripto.put(4, "Cardano");
+        cripto.put(5, "Solana");
+        
+        
+        
+        System.out.println("\n\n#1 forEach tradicional");
+        //  #1 forEach tradicional
+        for(Map.Entry<Integer, String> ejemplo:cripto.entrySet()){
+            System.out.println("Clave: "+ejemplo.getKey()+"   Valor: "+ejemplo.getValue());
+        }
+        
+        
+        System.out.println("#2 forEach");
+        //  #2 forEach
+        cripto.forEach((c, v) -> System.out.println("Clave: "+c+ "   Valor: "+v));
+        
+        
+        System.out.println("\n\n#3 API Stream de Java");
+        // #3 API Stream                entry = e
+        cripto.entrySet().stream().forEach(e -> System.out.println("Clave: "+e.getKey()+  "   Valor: "+e.getValue()));
+        
+        
+        
+        
+        
+    }
 }
