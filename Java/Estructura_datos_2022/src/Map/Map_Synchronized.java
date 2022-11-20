0268e491de1325e6fc64951acfd974d0014218af
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2022-11-19 19:00:06 -0600

    Mapa sincronizado

--- Java/Estructura_datos_2022/src/Map/Map_Synchronized.java
+++ Java/Estructura_datos_2022/src/Map/Map_Synchronized.java
@@ -1,6 +1,39 @@
-//  Ejemplo 3: Mapa sincronizado
+//  Ejemplo 3: Colecciones
 package Map;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Map_Synchronized {
-    
+    public static void main(String[] args) {
+        
+        Map<Integer, String> diccionarioHash = new HashMap<>();
+        
+        diccionarioHash.put(1, "Marco");
+        diccionarioHash.put(2, "Raul");
+        diccionarioHash.put(3, "Andres");
+        diccionarioHash.put(4, "Julio");
+        diccionarioHash.put(5, "Armando");
+        
+        
+        System.out.println("Uso de HashMap");
+        for(Entry<Integer, String> entry:diccionarioHash.entrySet()){
+            System.out.println("Clave: "+entry.getKey()+"   Valor: "+entry.getValue());
+        }
+        
+        System.out.println("\n\nUso de Tree");
+        
+        
+        
+        /*  Hashtable<c, v>   -   Aplicar bloqueo a nivel de objeto
+        HashMap hm = new HashMap();
+        HashMap hms = (HashMap) Collections.synchronizedMap(hm);    */
+        
+        
+        
+        
+        
+    }
 }
