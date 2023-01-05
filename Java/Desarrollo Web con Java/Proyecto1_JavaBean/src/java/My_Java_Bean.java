f0a971e91fa288dc9ca006c4fd370aa9c60bb29a
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-01-05 12:34:05 -0600

    Proyecto 1 - Java Bean

--- Java/Desarrollo Web con Java/Proyecto1_JavaBean/src/java/My_Java_Bean.java
+++ Java/Desarrollo Web con Java/Proyecto1_JavaBean/src/java/My_Java_Bean.java
@@ -1,7 +1,25 @@
+import java.io.Serializable;
 /**
  *
- * @author HP
+ * @author HP   -   MSS
  */
-public class My_Java_Bean {
+public class My_Java_Bean implements Serializable{  //  1.- Debe de ser Serializable
+    //  2.- Atributos privados
+    private String id;
+    private String name;
+    
+    
+    //  4.- Constructor vacio
+    public My_Java_Bean(){
+    }
+    
+    
+    //  3.- Sets and Gets
+    public String getId(){  return id;  }
+    public void setId(String id){   this.id = id;   }
+    
+    public String getName(){    return name;    }
+    public void setName(String name){   this.name = name;   }
+    
     
 }
