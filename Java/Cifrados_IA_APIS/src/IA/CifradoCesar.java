f1d9a54cf6a8ad9c255f7561e2b635ed381df304
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-03 23:02:34 -0600

    Cifrado de Cesar

--- Java/Cifrados_IA_APIS/src/IA/CifradoCesar.java
+++ Java/Cifrados_IA_APIS/src/IA/CifradoCesar.java
@@ -1,22 +1,24 @@
 /*
-    Este es un ejemplo básico de cifrado César. Ten en cuenta que este algoritmo es muy fácil de romper y no es seguro para usos reales de cifrado.
+    Este es un ejemplo básico de cifrado César. 
+    Este algoritmo es muy fácil de romper y no es seguro para usos reales de cifrado.
  */
 package IA;
-
 /**
- *
- * @author HP
+ * @author HP - MSS
  */
 import java.util.Scanner;
 
 public class CifradoCesar {
+    
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Introduce el texto a cifrar:");
+        System.out.println("*****   CIFRADO CESAR  *****");
+        System.out.print("Introduce el texto a cifrar: ");
         String textoPlano = scan.nextLine();
 
-        System.out.println("Introduce el número de desplazamientos:");
+        System.out.print("Introduce el número de desplazamientos: ");
         int desplazamientos = scan.nextInt();
 
         String textoCifrado = cifrar(textoPlano, desplazamientos);
@@ -26,25 +28,28 @@ public class CifradoCesar {
         System.out.println("Texto descifrado: " + textoDescifrado);
     }
 
+    
     public static String cifrar(String textoPlano, int desplazamientos) {
+        
         StringBuilder textoCifrado = new StringBuilder();
 
         for (int i = 0; i < textoPlano.length(); i++) {
             char c = (char) (textoPlano.charAt(i) + desplazamientos);
             textoCifrado.append(c);
         }
-
         return textoCifrado.toString();
     }
 
     public static String descifrar(String textoCifrado, int desplazamientos) {
+        
         StringBuilder textoDescifrado = new StringBuilder();
 
         for (int i = 0; i < textoCifrado.length(); i++) {
             char c = (char) (textoCifrado.charAt(i) - desplazamientos);
             textoDescifrado.append(c);
         }
-
         return textoDescifrado.toString();
     }
+    
+    
 }
