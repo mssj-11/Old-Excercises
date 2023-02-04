c5f2c6a601e3ca0b97d0b614fc68c06a78597a6b
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-03 23:25:41 -0600

    Cifrado de Vigenère

--- Java/Cifrados_IA_APIS/src/IA/CifradoVigenere.java
+++ Java/Cifrados_IA_APIS/src/IA/CifradoVigenere.java
@@ -1,22 +1,22 @@
 /*
-    Este es un ejemplo básico de cifrado de Vigenère. Al igual que en el ejemplo anterior, ten en cuenta que este algoritmo no es seguro para usos reales de cifrado y existen métodos más seguros para cifrar información.
+    Cifrado de Vigenère 
+    Este algoritmo no es seguro para usos reales de cifrado de informacion.
  */
 package IA;
-
 /**
- *
- * @author HP
+ * @author HP - MSS
  */
 import java.util.Scanner;
-
 public class CifradoVigenere {
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Introduce el texto a cifrar:");
+        System.out.println("*****   CIFRADO VIGENERE  *****");
+        System.out.print("Introduce el texto a cifrar: ");
         String textoPlano = scan.nextLine();
 
-        System.out.println("Introduce la clave:");
+        System.out.print("Introduce la clave: ");
         String clave = scan.nextLine();
 
         String textoCifrado = cifrar(textoPlano, clave);
@@ -25,6 +25,7 @@ public class CifradoVigenere {
         String textoDescifrado = descifrar(textoCifrado, clave);
         System.out.println("Texto descifrado: " + textoDescifrado);
     }
+    
 
     public static String cifrar(String textoPlano, String clave) {
         StringBuilder textoCifrado = new StringBuilder();
@@ -40,6 +41,7 @@ public class CifradoVigenere {
         return textoCifrado.toString();
     }
 
+    
     public static String descifrar(String textoCifrado, String clave) {
         StringBuilder textoDescifrado = new StringBuilder();
 
@@ -53,4 +55,6 @@ public class CifradoVigenere {
 
         return textoDescifrado.toString();
     }
+    
+    
 }
