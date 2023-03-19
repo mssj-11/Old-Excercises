48891b3acf03a0b623360c67646bc667a8843723
Mike <80673026+mssj-11@users.noreply.github.com>
2023-03-18 18:40:57 -0600

    Ejercicios de CF

---
+++ Python/ej1.py
@@ -0,0 +1,10 @@
+#   Definiendo la funcion que retornara un objeto de tipo entero
+def suma(a: int, b: int) -> int:
+    '''La suma de dos numeros'''
+    return a + b
+
+#   Llamando la funcion
+print(suma(10, 20))
+print(suma(55, 45))
+print(suma(100, 1))
+print(suma(110, 1))
