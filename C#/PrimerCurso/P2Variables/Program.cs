800fe8c16f47fd613e9acee7e5d0e64a7daab2d5
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-20 23:30:24 -0600

    Variables y Constantes

---
+++ C#/PrimerCurso/P2Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Varianbles
+            int numero = 55;
+            int numero2;
+
+            //  Constantes
+            const int constante = 21;
+        }
+    }
+}
